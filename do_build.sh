657144b916af45c48658b354fe258da70ba57fed
50kudos <50th.zaint@gmail.com>
2022-05-08 08:36:57 +0700

    Get rid of gzip in fset pkg

--- do_build.sh
+++ do_build.sh
@@ -9,8 +9,8 @@ cp -r ./lib/pkgs/fset ~/dev/product/fset/assets/js/internal/
 cp ./lib/pkgs/fset/dist/main.css ~/dev/product/fset/assets/css/vendor/fbox.css
 cp ./lib/pkgs/fset/dist/main.css.map ~/dev/product/fset/assets/css/vendor/fbox.css.map
 
-gzip -c ./lib/pkgs/fset/dist/main.js > ./lib/pkgs/fset/dist/fset.es.js.gz
-gzip -c ./lib/pkgs/fset/dist/main.css > ./lib/pkgs/fset/dist/fset.css.gz
+# gzip -c ./lib/pkgs/fset/dist/main.js > ./lib/pkgs/fset/dist/fset.es.js.gz
+# gzip -c ./lib/pkgs/fset/dist/main.css > ./lib/pkgs/fset/dist/fset.css.gz
 
 # Standalone
 gzip -c ./lib/pkgs/model/dist/model.es.js > ./lib/pkgs/model/dist/model.es.js.gz
